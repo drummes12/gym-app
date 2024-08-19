720c4bab5dd54ac7a80d185b75d8d727558d2d58
Esteban Gonzalez [drummes12] <drummes.12@gmail.com>
2024-08-19 16:30:56 -0500

    refactor: Update REST between sets and after exercise

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -21,3 +21,13 @@ export function createResponse(
     headers
   })
 }
+
+export function createIdFromString(str: string) {
+  return str
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-z0-9\s]/g, '')
+    .trim()
+    .replace(/\s+/g, '_')
+}
